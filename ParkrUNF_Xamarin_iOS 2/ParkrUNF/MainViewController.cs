90ab6e76523816851295e8156f77407c8cfae1bc
ismael-texidor <ismael.texidor@outlook.com>
2016-04-10 23:58:50 -0400

    Initial Push

---
+++ ParkrUNF_Xamarin_iOS 2/ParkrUNF/MainViewController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using UIKit;
+
+namespace ParkrUNF
+{
+	public partial class MainViewController : UIViewController
+	{
+		public UserDataService userDataService;
+		public UserDataItem user;
+		public MainViewController (IntPtr p) : base (p)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
+
